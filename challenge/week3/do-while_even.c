5b89f660658ec3631a5132c4932cc2818544bffc
jiiiwonyy <119236218+jiiiwonyy@users.noreply.github.com>
2023-09-18 16:32:13 +0900

    Update do-while_even.c

--- challenge/week3/do-while_even.c
+++ challenge/week3/do-while_even.c
@@ -7,11 +7,14 @@ int main() {
 		{
 			i++;
 			continue;
+			//continue문을 써서 조건에 맞으면 계속 실행되도록
 		}
 
 		printf("%d Hello world!\n", i);
 		i++;
+		// 홀수도 ++ 되도록함
 	} while (i < 10);
+	//i<10일때만 조건 수행
 
 	return 0;
 }
