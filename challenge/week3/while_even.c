196f4cdfc03cf4dba8a24d7f894b7a787c93e4a9
jiiiwonyy <119236218+jiiiwonyy@users.noreply.github.com>
2023-09-18 16:31:30 +0900

    Update while_even.c

--- challenge/week3/while_even.c
+++ challenge/week3/while_even.c
@@ -6,9 +6,11 @@ int main() {
 		if (i % 2 == 0) {
 			i++;
 			continue;
+			// continue문을 써서 조건에 맞으면 계속 실행되도록 설정
 		}
 		printf("%d Hello World!\n", i);
 		i++;
+		//홀수에도 ++를 해줘야 돼서 들어가야함
 	}
 	return 0;
 }
